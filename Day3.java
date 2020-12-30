a72368ac835c41a6be810d9d48ec702590c5a189
bepposax <43136113+bepposax@users.noreply.github.com>
2020-12-30 10:02:21 +0100

    day 3

---
+++ Day3.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day3 {
+    public static File data;
+    public static Scanner sc;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        data = new File("Day3.dat");
+
+        System.out.println("Part1 solution: " + solution1(3, 1));
+        System.out.println("Part2 solution: " + solution2());
+    }
+
+    public static long solution1(int rightHops, int downHops) throws FileNotFoundException {
+        sc = new Scanner(data);
+        int col = 0, trees = 0, length = sc.nextLine().length();
+
+        do {
+            col = (col + rightHops) % length;
+            for (int i = 1; i < downHops; i++)
+                sc.nextLine();
+            if (sc.nextLine().charAt(col) == '#')
+                trees++;
+        } while (sc.hasNextLine());
+
+        return trees;
+    }
+
+    public static long solution2() throws FileNotFoundException {
+        return solution1(1, 1) * solution1(3, 1) * solution1(5, 1) * solution1(7, 1) * solution1(1, 2);
+    }
+}
