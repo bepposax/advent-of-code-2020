3266a73ed25a56f0ebef31d638e4d5ec20233870
bepposax <43136113+bepposax@users.noreply.github.com>
2020-12-30 07:52:40 +0100

    part2 implemented

--- Day2.java
+++ Day2.java
@@ -3,13 +3,23 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Day2 {
-    public static void main(String[] args) throws FileNotFoundException {
-        File data = new File("Day2.dat");
-        Scanner sc = new Scanner(data);
+    public static File data;
+    public static Scanner sc;
+    public static int min, max;
+    public static String password;
+    public static String[] line;
 
-        int min, max, validPasswords = 0;
-        String password;
-        String[] line = new String[4];
+    public static void main(String[] args) throws FileNotFoundException {
+        data = new File("Day2.dat");
+        line = new String[4];
+
+        System.out.print("Part 1 solution: " + validate1() + "\nPart 2 solution: " + validate2());
+        sc.close();
+    }
+
+    public static int validate1() throws FileNotFoundException {
+        int validPwds = 0;
+        sc = new Scanner(data);
 
         while (sc.hasNextLine()) {
             line = sc.nextLine().replace(":", "").split(" |-");
@@ -19,10 +29,25 @@ public class Day2 {
 
             int num = password.length(); // so the occurrences of that letter becomes equal to the length
             if (num >= min && num <= max)
-                validPasswords++;
+                validPwds++;
         }
-        sc.close();
+        return validPwds;
+    }
 
-        System.out.print(validPasswords);
+    public static int validate2() throws FileNotFoundException {
+        int validPwds = 0;
+        sc = new Scanner(data);
+
+        while (sc.hasNextLine()) {
+            line = sc.nextLine().replace(":", "").split(" |-");
+            min = Integer.parseInt(line[0]);
+            max = Integer.parseInt(line[1]);
+            char letter = line[2].charAt(0);
+            password = line[3];
+
+            if ((password.charAt(min - 1) == letter) ^ (password.charAt(max - 1) == letter))
+                validPwds++;
+        }
+        return validPwds;
     }
 }
