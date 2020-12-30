1ec3737a5215b19f480dbcbeebab0e1c47a67f6e
bepposax <43136113+bepposax@users.noreply.github.com>
2020-12-30 08:05:01 +0100

    refactor

--- Day2.java
+++ Day2.java
@@ -6,6 +6,7 @@ public class Day2 {
     public static File data;
     public static Scanner sc;
     public static int min, max;
+    public static char letter;
     public static String password;
     public static String[] line;
 
@@ -16,18 +17,24 @@ public class Day2 {
         System.out.print("Part 1 solution: " + validate1() + "\nPart 2 solution: " + validate2());
     }
 
+    public static void read() throws FileNotFoundException {
+        line = sc.nextLine().replace(":", "").split(" |-");
+        min = Integer.parseInt(line[0]);
+        max = Integer.parseInt(line[1]);
+        letter = line[2].charAt(0);
+        password = line[3];
+    }
+
     public static int validate1() throws FileNotFoundException {
         int validPwds = 0;
         sc = new Scanner(data);
 
         while (sc.hasNextLine()) {
-            line = sc.nextLine().replace(":", "").split(" |-");
-            min = Integer.parseInt(line[0]);
-            max = Integer.parseInt(line[1]);
-            password = line[3].replaceAll("[^" + line[2] + "]", ""); // changes the pw to contain only the given letter
-
-            int num = password.length(); // so the occurrences of that letter becomes equal to the length
-            if (num >= min && num <= max)
+            read();
+            // change the pw to contain only the given letter so the occurrences of that
+            // letter becomes equal to the length
+            int occurrences = password.replaceAll("[^" + letter + "]", "").length();
+            if (occurrences >= min && occurrences <= max)
                 validPwds++;
         }
         sc.close();
@@ -39,12 +46,7 @@ public class Day2 {
         sc = new Scanner(data);
 
         while (sc.hasNextLine()) {
-            line = sc.nextLine().replace(":", "").split(" |-");
-            min = Integer.parseInt(line[0]);
-            max = Integer.parseInt(line[1]);
-            char letter = line[2].charAt(0);
-            password = line[3];
-
+            read();
             if ((password.charAt(min - 1) == letter) ^ (password.charAt(max - 1) == letter))
                 validPwds++;
         }
