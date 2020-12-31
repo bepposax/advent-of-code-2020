af1cc0bed175eb79ec3d000950c2097f9b5a099c
bepposax <43136113+bepposax@users.noreply.github.com>
2020-12-31 06:19:12 +0100

    Update Day4.java
    part2

--- Day4.java
+++ Day4.java
@@ -10,6 +10,7 @@ public class Day4 {
         data = new File("Day4.dat");
 
         System.out.println("Part1 solution: " + solution1());
+        System.out.println("Part2 solution: " + solution2());
     }
 
     private static int solution1() throws FileNotFoundException {
@@ -24,6 +25,91 @@ public class Day4 {
                 for (String s : line.split(" "))
                     if (!s.split(":")[0].equals("cid"))
                         fields++;
+            } else {
+                if (fields == 7)
+                    validPassports++;
+                fields = 0;
+            }
+        }
+        if (fields == 7) // last line
+            validPassports++;
+
+        sc.close();
+        return validPassports;
+    }
+
+    private static int solution2() throws FileNotFoundException {
+        sc = new Scanner(data);
+        int validPassports = 0, fields = 0;
+        String line;
+
+        while (sc.hasNextLine()) {
+            line = sc.nextLine();
+
+            if (!line.isEmpty()) {
+                for (String s : line.split(" ")) {
+                    String[] field = s.split(":");
+                    String key = field[0], value = field[1];
+
+                    if (!key.equals("cid"))
+                        switch (key) {
+                            case ("byr"):
+                                if (value.length() == 4) {
+                                    int year = Integer.parseInt(value);
+                                    if (year >= 1920 && year <= 2002)
+                                        fields++;
+                                }
+                                break;
+                            case ("iyr"):
+                                if (value.length() == 4) {
+                                    int year = Integer.parseInt(value);
+                                    if (year >= 2010 && year <= 2020)
+                                        fields++;
+                                }
+                                break;
+                            case ("eyr"):
+                                if (value.length() == 4) {
+                                    int year = Integer.parseInt(value);
+                                    if (year >= 2020 && year <= 2030)
+                                        fields++;
+                                }
+                                break;
+                            case ("hgt"):
+                                String unity = value.substring(value.length() - 2);
+                                if (!(unity.equals("cm") || unity.equals("in")))
+                                    break;
+                                int quantity = Integer.parseInt(value.substring(0, value.length() - 2));
+                                int max, min;
+
+                                if (unity.equals("cm")) {
+                                    min = 150;
+                                    max = 193;
+                                } else if (unity.equals("in")) {
+                                    min = 59;
+                                    max = 76;
+                                } else
+                                    break;
+                                if (quantity >= min && quantity <= max)
+                                    fields++;
+                                break;
+                            case ("hcl"):
+                                if (value.matches("^#[[0-9]|[a-f]]{6}"))
+                                    fields++;
+                                break;
+                            case ("ecl"):
+                                String[] colors = { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+                                for (String color : colors)
+                                    if (value.equals(color)) {
+                                        fields++;
+                                        break;
+                                    }
+                                break;
+                            case ("pid"):
+                                if (value.matches("[0-9]{9}"))
+                                    fields++;
+                                break;
+                        }
+                }
             } else {
                 if (fields == 7)
                     validPassports++;
